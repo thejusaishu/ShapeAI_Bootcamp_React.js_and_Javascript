3b27ed98c3ad56489791dc922be687d5ec1db9e0
thejusaishu <86412121+thejusaishu@users.noreply.github.com>
2021-06-26 23:12:00 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note"> 
+      <h1>Javascript and React.js</h1>
+      <p>This was an amazing bootcamp taken up by Shaurya Sinha.We coveredd everything from scratch incuding Javascript,React.js,HTML</p>
+    </div>   
+  );
+}
+
+export default Info;
